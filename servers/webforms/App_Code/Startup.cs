20eefa7d308287f37a8cd8230f9920e7b6d74452
mukulk <mukulk@outlook.com>
2017-07-29 11:18:50 +1000

    New webforms project

---
+++ servers/webforms/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webforms.Startup))]
+namespace webforms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
